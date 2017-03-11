ec50e271b94309edd62eb9ee43e312709f5f89d6
YafimK <fkpkot@gmail.com>
2017-03-11 08:51:06 +0200

    basic chat windows setup basic connection to layout from chat activity

--- simpleChat/app/src/main/java/com/kazak/simplechat/ChatWindow.java
+++ simpleChat/app/src/main/java/com/kazak/simplechat/ChatWindow.java
@@ -2,12 +2,26 @@ package com.kazak.simplechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ChatWindow extends AppCompatActivity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+    private boolean side = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+
+        buttonSend = (Button) findViewById(R.id.send);
+        listView = (ListView) findViewById(R.id.msgview);
+
     }
 }
