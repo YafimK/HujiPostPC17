887f4dc3fd62ada587f1c3ae9669afc7d1f931a7
YafimK <fkpkot@gmail.com>
2017-03-13 22:16:02 +0200

    basic chat windows setup basic connection to layout from chat activity

---
+++ simpleChat/app/src/main/java/com/kazak/simplechat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.kazak.simplechat;
+
+/**
+ * Created by fimka on 11/03/2017.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
