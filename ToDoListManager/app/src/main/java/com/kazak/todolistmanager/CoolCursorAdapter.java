2aeeffae2ca12eec94f068ffbbf5328f19a8e932
YafimK <fkpkot@gmail.com>
2017-03-22 18:42:34 +0200

    Added the basic todo list manager

---
+++ ToDoListManager/app/src/main/java/com/kazak/todolistmanager/CoolCursorAdapter.java
@@ -0,0 +1,29 @@
+package com.kazak.todolistmanager;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by fimka on 22/03/2017.
+ */
+
+class CoolCursorAdapter extends SimpleCursorAdapter {
+    public CoolCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+        super(context, layout, c, from, to, flags);
+    }
+
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
+        if (position == 10) {
+            // set background color = red;
+        } else {
+            // set background color = green;
+        }
+        return view;
+    }
+}
---
+++ ToDoListManager/app/src/main/java/com/kazak/todolistmanager/MainActivity.java
@@ -0,0 +1,134 @@
+package com.kazak.todolistmanager;
+
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+    private ListAdapter todoListAdapter;
+    private TodoListSQLHelper todoListSQLHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final MainActivity self = this;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder todoTaskBuilder = new AlertDialog.Builder(self);
+                todoTaskBuilder.setTitle("Add Todo Task Item");
+                todoTaskBuilder.setMessage("describe the Todo task...");
+                final EditText todoET = new EditText(self);
+                todoTaskBuilder.setView(todoET);
+                todoTaskBuilder.setPositiveButton("Add Task", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String todoTaskInput = todoET.getText().toString();
+                        todoListSQLHelper = new TodoListSQLHelper(self);
+                        SQLiteDatabase sqLiteDatabase = todoListSQLHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.clear();
+
+                        //write the Todo task input into database table
+                        values.put(TodoListSQLHelper.COL1_TASK, todoTaskInput);
+                        sqLiteDatabase.insertWithOnConflict(TodoListSQLHelper.TABLE_NAME, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+
+                        //update the Todo task list UI
+                        updateTodoList();
+
+                    }
+
+                });
+                todoTaskBuilder.setNegativeButton("Cancel", null);
+
+                todoTaskBuilder.create().show();
+            }
+        });
+
+
+    }
+
+    private void updateTodoList() {
+        todoListSQLHelper = new TodoListSQLHelper(this);
+        SQLiteDatabase sqLiteDatabase = todoListSQLHelper.getReadableDatabase();
+
+        //cursor to read todo task list from database
+        Cursor cursor = sqLiteDatabase.query(TodoListSQLHelper.TABLE_NAME,
+                new String[]{TodoListSQLHelper._ID, TodoListSQLHelper.COL1_TASK},
+                null, null, null, null, null);
+
+        //binds the todo task list with the UI
+        todoListAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task,
+                cursor,
+                new String[]{TodoListSQLHelper.COL1_TASK},
+                new int[]{R.id.todoTaskTV},
+                0
+        );
+
+        this.setListAdapter(todoListAdapter);
+    }
+
+
+    //closing the todo task item
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView todoTV = (TextView) v.findViewById(R.id.todoTaskTV);
+        String todoTaskItem = todoTV.getText().toString();
+
+        String deleteTodoItemSql = "DELETE FROM " + TodoListSQLHelper.TABLE_NAME +
+                " WHERE " + TodoListSQLHelper.COL1_TASK + " = '" + todoTaskItem + "'";
+
+        todoListSQLHelper = new TodoListSQLHelper(this);
+        SQLiteDatabase sqlDB = todoListSQLHelper.getWritableDatabase();
+        sqlDB.execSQL(deleteTodoItemSql);
+        updateTodoList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
